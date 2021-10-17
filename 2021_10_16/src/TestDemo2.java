618b1bc031c65122c92bea30db13e6cf5afbcf37
xwj <1983318249@qq.com>
2021-10-17 19:12:46 +0800

    17

---
+++ 2021_10_16/src/TestDemo2.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:  19833
+ * Date:  2021-10-16
+ * Time:  13:11
+ */
+
+public class TestDemo2{
+    public static void main(String[] args) {
+
+        int a = 10;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(123456);
+            int ret = function(i);
+        }
+        System.out.println(a);
+
+    }
+    public static int function(int i){
+        return i;
+    }
+}
