746af8e4e0c15c81645ff44190c7e76e6b12aa2a
xwj <1983318249@qq.com>
2021-11-20 22:56:13 +0800

    web项目

---
+++ WebProject/src/Demo/Demo3.java
@@ -0,0 +1,25 @@
+package Demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/do3")
+public class Demo3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.setContentType("text/html;charset=utf-8");
+        PrintWriter write = resp.getWriter();
+
+        write.write("<h1>你好！！！</h1>");
+    }
+}
